5254cb0cf3b4509b1a5107e71a148c21be94ce5f
Johnny Gale <c18jonathan.gale@gmail.com>
2024-03-01 15:53:06 +0000

    init for chinese checkers

--- marl/chinesecheckers.py
+++ marl/chinesecheckers.py
@@ -5,7 +5,20 @@ import gymnasium as gym
 from gymnasium import spaces
 
 class ChineseCheckersEnv(gym.Env):
-    metadata = {"render_modes": []}
+    metadata = {
+        "render_modes": ["human"],
+    }
+
+    COLORS = {
+        "BACKGROUND": (255, 255, 255),
+        "EMPTY":      (139, 139, 139),
+        "P1":         (0,   0,   0  ),
+        "P2":         (255, 255, 255),
+        "P3":         (255, 0,   0  ),
+        "P4":         (0,   0,   255),
+        "P5":         (0,   255, 0  ),
+        "P6":         (255, 255, 0  ),
+    }
 
     def __init__(self, render_mode=None, players=2):
         self.window_size = 512 # Size of the PyGame window
@@ -19,4 +32,16 @@ class ChineseCheckersEnv(gym.Env):
         assert render_mode is None or render_mode in self.metadata["render_modes"]
         self.render_mode = render_mode
 
+    def _render_frame(self):
+        if self.window is None and self.render_mode == "human":
+            pygame.init()
+            pygame.display.init()
+            self.window = pygame.display.set_mode(
+                (self.window_size, self.window_size)
+            )
+        
+        canvas = pygame.Surface((self.window_size, self.window_size))
+        canvas.fill(self.COLORS["BACKGROUND"])
+        
+        
         
