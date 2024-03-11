6406b8866b28a2c692e172ec4345f3a2863f409d
JohnnyJones <c18jonathan.gale@gmail.com>
2024-03-11 17:06:57 -0400

    implement DeterministicGreedyAgent

--- marl/agent.py
+++ marl/agent.py
@@ -7,6 +7,7 @@ import random
 
 class ChineseCheckersAgent():
     Action = namedtuple("Action", ["piece_id", "position"])
+    Position = namedtuple("Position", ["x", "y"])
 
     def __init__(self) -> None:
         raise NotImplementedError("ChineseCheckersAgent is an abstract class")
@@ -33,14 +34,59 @@ class RandomAgent(ChineseCheckersAgent):
 
 class DeterministicGreedyAgent(ChineseCheckersAgent):
     def __init__(self) -> None:
-        super(DeterministicGreedyAgent, self).__init__()
-    
-    def act(self, observation):
-        super(DeterministicGreedyAgent, self).act(observation)
-        return self._get_best_action(observation)
-    
-    def _get_best_action(self, observation):
         pass
+    
+    def act(self, observation, info) -> ChineseCheckersAgent.Action:
+        super(DeterministicGreedyAgent, self).act(observation, info)
+        return self._get_best_action(observation, info)
+    
+    def _get_best_action(self, observation, info) -> ChineseCheckersAgent.Action:
+        actions = info["valid_actions_list"]
+        if len(actions) == 0:
+            raise ValueError("No valid actions available")
+        
+        heuristics = [self._heuristic(observation, action, info) for action in actions]
+        # find all the actions with the best heuristic
+        best_heuristic = max(heuristics)
+        best_actions = [action for action, heuristic in zip(actions, heuristics) if heuristic == best_heuristic]
+
+        if len(best_actions) == 1:
+            return best_actions[0]
+        else:
+            # we have multiple best actions
+            # find furthest behind piece
+            def distance(player, piece_id):
+                position = ChineseCheckersAgent.Position(*info["id_to_position"][player][piece_id])
+                distance = position.x - position.y
+                if player == 1:
+                    distance = -distance
+                return distance
+
+            # sort the best actions by the distance of the piece
+            # index 0 will be the furthest behind piece
+            best_piece_ids = [action.piece_id for action in best_actions]
+            best_piece_ids.sort(key=lambda piece_id: distance(info["turn"], piece_id))
+
+            # sample actions belonging to the best piece
+            best_piece_actions = [action for action in best_actions if action.piece_id == best_piece_ids[0]]
+            best_action = random.choice(best_piece_actions)
+
+        return best_action
+
+    
+    def _heuristic(self, observation, action: ChineseCheckersAgent.Action, info):
+        # Moving further is better
+        turn = info["turn"]
+        starting_position = ChineseCheckersAgent.Position(*info["id_to_position"][turn][action.piece_id])
+        ending_position = action.position
+
+        # due the ending positions being at the top right and bottom left of the board matrix,
+        # higher x is better, lower y is better for player 0
+        # lower x is better, higher y is better for player 1
+        heuristic = (starting_position.x - starting_position.y) - (ending_position.x - ending_position.y)
+        if turn == 1:
+            heuristic = -heuristic
+        return heuristic
 
 class StochasticGreedyAgent(DeterministicGreedyAgent):
     def __init__(self) -> None:
