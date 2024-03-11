da425758d1089afa31d3f9a5253a44853a1f910d
JohnnyJones <c18jonathan.gale@gmail.com>
2024-03-11 15:59:17 -0400

    add board neighbor checking, return valid actions in info

--- marl/chinesecheckers.py
+++ marl/chinesecheckers.py
@@ -26,6 +26,7 @@ class ChineseCheckersEnv(gym.Env):
 
     class Board: 
         class Position(namedtuple("Position", ["x", "y"])): pass
+        class Action(namedtuple("Action", ["piece_id", "position"])): pass
 
         # game board is 7x7 matrix (diamond-shaped board) with 2 players
         def __init__(self):
@@ -99,30 +100,137 @@ class ChineseCheckersEnv(gym.Env):
                 return 1
             return -1
 
-        def get_valid_actions(self, player_id):
+        def get_action_mask(self, player_id):
             moves = np.zeros((6, 7, 7), dtype=np.bool_)
             
             # rolling
             for piece_id in range(6):
                 # check adjacent positions
                 position = self.Position(*self.id_to_position[player_id, piece_id])
-                for dx in range(-1, 2): # -1, 0, 1
-                    for dy in range(-1, 2):
-                        if dx == 0 and dy == 0:
-                            continue
-                        new_position = self.Position(position.x + dx, position.y + dy)
-                        if 0 <= new_position.x < 7 and 0 <= new_position.y < 7 and self.board[new_position] == -1:
-                            moves[piece_id, new_position.x, new_position.y] = True
-            
-            # hopping with DFS
-            for piece_id in range(6):
-                position = self.Position(*self.id_to_position[player_id, piece_id])
-                # self._dfs(moves, player_id, piece_id, position, position, 0)
+                adjacent_positions = self.get_reachable_positions(position)
+                for adjacent_position in adjacent_positions:
+                    moves[piece_id, adjacent_position.x, adjacent_position.y] = True
 
             return moves
+        
+        def get_valid_actions_list(self, player_id):
+            actions = []
+            for piece_id in range(6):
+                position = self.Position(*self.id_to_position[player_id, piece_id])
+                reachable_positions = self.get_reachable_positions(position)
+                for reachable_position in reachable_positions:
+                    action = self.Action(piece_id, 
+                                         self.Position(reachable_position.x, reachable_position.y))
+                    actions.append(action)
+            return actions
 
+        def get_valid_actions_dict(self, player_id):
+            actions = {}
+            for piece_id in range(6):
+                position = self.Position(*self.id_to_position[player_id, piece_id])
+                reachable_positions = self.get_reachable_positions(position)
+                for reachable_position in reachable_positions:
+                    if piece_id not in actions:
+                        actions[piece_id] = [reachable_position]
+                    else:
+                        actions[piece_id].append(reachable_position)
+            return actions
 
+        def get_adjacent_positions(self, position: Position) -> list[Position]:
+            positions = []
+            for dx in range(-1, 2):
+                for dy in range(-1, 2):
+                    if dx + dy == 0:
+                        continue
+                    new_position = self.Position(position.x + dx, position.y + dy)
+                    if 0 <= new_position.x < 7 and 0 <= new_position.y < 7:
+                        positions.append(new_position)
+        
+        def get_unoccupied_adjacent_positions(self, position: Position) -> list[Position]:
+            positions = []
+            for dx in range(-1, 2):
+                for dy in range(-1, 2):
+                    if dx + dy == 0:
+                        continue
+                    new_position = self.Position(position.x + dx, position.y + dy)
+                    if 0 <= new_position.x < 7 and 0 <= new_position.y < 7 and self.board[new_position] == -1:
+                        positions.append(new_position)
+            return positions
+        
+        def get_occupied_adjacent_positions(self, position: Position) -> list[Position]:
+            positions = []
+            for dx in range(-1, 2):
+                for dy in range(-1, 2):
+                    if dx + dy == 0:
+                        continue
+                    new_position = self.Position(position.x + dx, position.y + dy)
+                    if 0 <= new_position.x < 7 and 0 <= new_position.y < 7 and self.board[new_position] != -1:
+                        positions.append(new_position)
+            return positions
+        
+        def get_hop_positions(self, position: Position) -> list[Position]:
+            positions = []
+            for dx in range(-1, 2):
+                for dy in range(-1, 2):
+                    if dx + dy == 0:
+                        continue
+                    adj_position = self.Position(position.x + dx, position.y + dy)
+                    hop_position = self.Position(position.x + 2 * dx, position.y + 2 * dy)
+                    if (0 <= hop_position.x < 7 and 0 <= hop_position.y < 7 and
+                        self.board[adj_position] != -1):
+                        positions.append(hop_position)
+            return positions
 
+        def get_unoccupied_hop_positions(self, position: Position) -> list[Position]:
+            positions = []
+            for dx in range(-1, 2):
+                for dy in range(-1, 2):
+                    if dx + dy == 0:
+                        continue
+                    adj_position = self.Position(position.x + dx, position.y + dy)
+                    hop_position = self.Position(position.x + 2 * dx, position.y + 2 * dy)
+                    if (0 <= hop_position.x < 7 and 0 <= hop_position.y < 7 and
+                        self.board[adj_position] != -1 and self.board[hop_position] == -1):
+                        positions.append(hop_position)
+            return positions
+        
+        def get_reachable_positions(self, position: Position) -> list[Position]:
+            positions = []
+            
+            # recursive hopping using depth-first search
+            def hop_dfs(position, visited: set = None, stack: list = None):
+                if visited is None:
+                    visited = set()
+                if stack is None:
+                    stack = []
+                
+                reachable = []
+
+                # can be multiple ways to reach same position
+                if position in visited:
+                    return reachable
+                visited.add(position)
+                
+                neighbors = self.get_unoccupied_hop_positions(position)
+                for neighbor in neighbors:
+                    if neighbor not in visited:
+                        stack.append(neighbor)
+                        reachable.append(neighbor)
+                
+                while stack:
+                    new_position = stack.pop()
+                    reachable.extend(hop_dfs(new_position, visited, stack))
+
+                return reachable
+
+            # hopping positions
+            positions.extend(hop_dfs(position))
+
+            # rolling positions
+            positions.extend(self.get_unoccupied_adjacent_positions(position))
+            
+            return positions
+        
         def observation(self):
             return self.position_to_id
 
@@ -169,6 +277,9 @@ class ChineseCheckersEnv(gym.Env):
     def _get_info(self):
         return {
             "turn": f"P{self.turn}",
+            "action_mask": self.board.get_action_mask(self.turn),
+            "valid_actions_dict": self.board.get_valid_actions_dict(self.turn),
+            "valid_actions_list": self.board.get_valid_actions_list(self.turn),
         }
 
     def reset(self, seed=None, options=None):
@@ -183,6 +294,11 @@ class ChineseCheckersEnv(gym.Env):
     
     def step(self, action):
         # TODO: check if action is valid
+        piece_id, move_position = action
+        move_position = self.board.Position(*move_position)
+        valid_actions = self.board.get_action_mask(self.turn)
+        if not valid_actions[piece_id, move_position.x, move_position.y]:
+            raise ValueError("Invalid action")
         self.board.move_piece(*(self.turn, *action))
 
         reward = 0
